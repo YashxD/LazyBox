4ac05be09e3a1477f9a3112c00b81c45a38c2f41
MKND <47962010+bizzarioth@users.noreply.github.com>
2019-03-04 18:10:25 +0530

    Updated

--- app/src/main/java/io/github/yashxd/lazybox/MainActivity.java
+++ app/src/main/java/io/github/yashxd/lazybox/MainActivity.java
@@ -2,12 +2,39 @@ package io.github.yashxd.lazybox;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button grbt;
+    TextView powtv;
+    Switch swch1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        grbt=(Button)findViewById(R.id.graph_btn);
+        powtv=(TextView)findViewById(R.id.power_tv);
+        swch1=(Switch)findViewById(R.id.tog1);
+
+        swch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                //send ping to server
+                Toast.makeText(getApplicationContext(),"Switch toggled",Toast.LENGTH_SHORT).show();
+            }
+        });
+        grbt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //start graph activity
+            }
+        });
+
     }
+
 }
--- app/src/main/java/io/github/yashxd/lazybox/PowerHistory.java
+++ app/src/main/java/io/github/yashxd/lazybox/PowerHistory.java
@@ -3,11 +3,12 @@ package io.github.yashxd.lazybox;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class PowerActivity extends AppCompatActivity {
+public class PowerHistory extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_power);
+        setContentView(R.layout.activity_power_history);
+        
     }
 }
