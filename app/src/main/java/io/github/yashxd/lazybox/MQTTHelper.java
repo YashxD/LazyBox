edf039aba4b78144fa0edc6c7a300d66e52f1f68
Yash Jain <yashjain.1999@gmail.com>
2019-04-11 20:45:10 +0530

    Added Basic MQTT Funstionality

---
+++ app/src/main/java/io/github/yashxd/lazybox/MQTTHelper.java
@@ -0,0 +1,148 @@
+/**
+ * Source -
+ * https://wildanmsyah.wordpress.com/2017/05/11/mqtt-android-client-tutorial/
+ */
+
+
+package io.github.yashxd.lazybox;
+
+import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.DisconnectedBufferOptions;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+
+public class MQTTHelper {
+    public MqttAndroidClient mqttAndroidClient;
+
+    //final String serverUri = "mqtts://azureRath:1215582ae9a44a3f9070665bca45db44:io.adafruit.com";
+    /*final String serverUri = "io.adafruit.com:1883";
+    final String clientId = MqttClient.generateClientId();
+    final String subscriptionTopic = "azureRath/f/esp32.output";
+
+    final String username = "azureRath";
+    final String password = "1215582ae9a44a3f9070665bca45db44";*/
+
+    final String serverUri = "tcp://m24.cloudmqtt.com:14818";
+
+    final String clientId = "ExampleAndroidClient";
+    final String subscriptionTopic = "sensor/lol";
+
+    final String username = "gldsqnue";
+    final String password = "3IjECOD6a7It";
+
+    final String TAG = "MQTTHelper";
+
+    private Context context;
+
+    public MQTTHelper(Context context){
+        mqttAndroidClient = new MqttAndroidClient(context, serverUri, clientId);
+        mqttAndroidClient.setCallback(new MqttCallbackExtended() {
+            @Override
+            public void connectComplete(boolean b, String s) {
+                Log.w(TAG, s);
+            }
+
+            @Override
+            public void connectionLost(Throwable throwable) {
+
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage mqttMessage) throws Exception {
+                Log.w(TAG, mqttMessage.toString());
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
+
+            }
+        });
+        connect();
+        this.context = context;
+    }
+
+    public void setCallback(MqttCallbackExtended callback) {
+        mqttAndroidClient.setCallback(callback);
+    }
+
+    private void connect(){
+        MqttConnectOptions mqttConnectOptions = new MqttConnectOptions();
+        mqttConnectOptions.setAutomaticReconnect(true);
+        mqttConnectOptions.setCleanSession(false);
+        mqttConnectOptions.setUserName(username);
+        mqttConnectOptions.setPassword(password.toCharArray());
+        //mqttConnectOptions.setServerURIs(serverUri.toCharArray());
+
+        try {
+            //mqttAndroidClient.connect(mqttConnectOptions);
+            //Toast.makeText(context, "Connected to MQTT Server", Toast.LENGTH_SHORT).show();
+
+            mqttAndroidClient.connect(mqttConnectOptions, null, new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+
+                    DisconnectedBufferOptions disconnectedBufferOptions = new DisconnectedBufferOptions();
+                    disconnectedBufferOptions.setBufferEnabled(true);
+                    disconnectedBufferOptions.setBufferSize(100);
+                    disconnectedBufferOptions.setPersistBuffer(false);
+                    disconnectedBufferOptions.setDeleteOldestMessages(false);
+                    mqttAndroidClient.setBufferOpts(disconnectedBufferOptions);
+
+                    Toast.makeText(context, "Connected to MQTT Server", Toast.LENGTH_SHORT).show();
+                    Log.d(TAG, "Connected to MQTT Server");
+                    subscribeToTopic();
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    Log.w(TAG, "Failed to connect to: " + serverUri + exception.toString());
+                }
+            });
+
+
+        } catch (MqttException ex){
+            ex.printStackTrace();
+            Log.e(TAG,ex.getMessage());
+        }
+    }
+
+
+    private void subscribeToTopic() {
+        try {
+            mqttAndroidClient.subscribe(subscriptionTopic, 0, null, new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    Log.w(TAG,"Subscribed!");
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    Log.w(TAG, "Subscribed fail!");
+                }
+            });
+
+        } catch (MqttException ex) {
+            System.err.println("Exception whilst subscribing");
+            ex.printStackTrace();
+        }
+    }
+    public void publishMessage(String payload) throws MqttException {
+        MqttMessage message = new MqttMessage(payload.getBytes());
+        mqttAndroidClient.publish(subscriptionTopic, message);
+    }
+    public void disconnect() throws MqttException {
+        mqttAndroidClient.disconnect();
+        Log.d(TAG, "Disconnected Successfully");
+    }
+}
--- app/src/main/java/io/github/yashxd/lazybox/MainActivity.java
+++ app/src/main/java/io/github/yashxd/lazybox/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.LauncherActivity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
@@ -11,15 +12,22 @@ import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
 public class MainActivity extends AppCompatActivity {
 
     Button switchButton;
     Button powerHistoryButton;
-    Button connectButton;
+    Button configButton;
 
     Boolean switchState = false;
 
     String TAG = "MainActivity";
+
+    MQTTHelper mqttHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
 
         switchButton = findViewById(R.id.button_switch_activity_main);
         powerHistoryButton = findViewById(R.id.button_history_activity_main);
-        connectButton = findViewById(R.id.button_connect_activity_main);
+        configButton = findViewById(R.id.button_config_activity_main);
 
         //Initialize button in off state
         switchButton.setBackgroundColor(getResources().getColor(R.color.colorButtonOff));
@@ -38,13 +46,32 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 switchState = !switchState;
+                /*try {
+                    mqttHelper.disconnect();
+                } catch (MqttException e) {
+                    e.printStackTrace();
+                }*/
                 if(switchState){
-                    switchButton.setBackgroundColor(getResources().getColor(R.color.colorButtonOn));
-                    switchButton.setText(getText(R.string.switch_on));
+                    try {
+                        mqttHelper.publishMessage("1");
+                        switchButton.setBackgroundColor(getResources().getColor(R.color.colorButtonOn));
+                        switchButton.setText(getText(R.string.switch_on));
+                        Toast.makeText(getApplicationContext(),"Payload delivered", Toast.LENGTH_SHORT).show();
+                    } catch (MqttException e) {
+                        e.printStackTrace();
+                        Toast.makeText(getApplicationContext(),"Error delivering payload", Toast.LENGTH_SHORT).show();
+                    }
                 }
                 else{
-                    switchButton.setBackgroundColor(getResources().getColor(R.color.colorButtonOff));
-                    switchButton.setText(getText(R.string.switch_off));
+                    try {
+                        mqttHelper.publishMessage("0");
+                        switchButton.setBackgroundColor(getResources().getColor(R.color.colorButtonOff));
+                        switchButton.setText(getText(R.string.switch_off));
+                        Toast.makeText(getApplicationContext(),"Payload delivered", Toast.LENGTH_SHORT).show();
+                    } catch (MqttException e) {
+                        e.printStackTrace();
+                        Toast.makeText(getApplicationContext(),"Error delivering payload", Toast.LENGTH_SHORT).show();
+                    }
                 }
             }
         });
@@ -57,7 +84,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        connectButton.setOnClickListener(new View.OnClickListener() {
+        configButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getApplicationContext(),"Coming Soon!",Toast.LENGTH_SHORT).show();
@@ -65,4 +92,35 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        startMQTT();
+    }
+
+    private void startMQTT(){
+        mqttHelper = new MQTTHelper(getApplicationContext());
+        mqttHelper.setCallback(new MqttCallbackExtended() {
+            @Override
+            public void connectComplete(boolean b, String s) {
+
+            }
+
+            @Override
+            public void connectionLost(Throwable throwable) {
+
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage mqttMessage) throws Exception {
+                Log.w(TAG,mqttMessage.toString());
+                Toast.makeText(getApplicationContext(), mqttMessage.toString(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
+
+            }
+        });
+    }
 }
